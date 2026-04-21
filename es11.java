e596e0bd2b0d7785dbc9e6f1f13adf3dfdf50f14
studcastelnuovoa-3a2026 <stud.castelnuovoa@gdromagnosi.edu.it>
2026-04-21 10:48:59 +0000

    aggiungi es10.java

--- es11.java
+++ es11.java
@@ -1,22 +1,25 @@
-6
 import java.io.*;
-public class es10 {
+public class es11 {
 public static void main (String[] args) throws Exception{
 BufferedReader input = new BufferedReader (new InputStreamReader(System.in));
 int n;
-int tab;
-
+int s=0;
+int i=0;
+int v=0;
 do{
 System.out.println ("dammi il numero ");
 String str= input.readLine();
 n=Integer.parseInt(str);
-int i=0;
-while(i<=10){ 
-tab=n*i;
-System.out.println (tab);
-i=i+1;6
+
+while(i<=n){
+s=s+i;
+System.out.println (s);
+
+i=i+1;
 
 }
 }
-while (n!=0);
+while (n!=-1);
 }}
+
+}
