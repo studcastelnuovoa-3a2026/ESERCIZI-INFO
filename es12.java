4655d23d7ad9cddcbafb041f6b089c92aeb8a80c
studcastelnuovoa-3a2026 <stud.castelnuovoa@gdromagnosi.edu.it>
2026-04-28 10:29:45 +0000

    aggiungi es12.java

--- es12.java
+++ es12.java
@@ -3,12 +3,16 @@ public class es12 {
 public static void main (String[] args) throws Exception{
 BufferedReader input = new BufferedReader (new InputStreamReader(System.in));
 int num;
+int tot=0;
 do{
 System.out.println ("dammi il numero ");
 String str= input.readLine();
 num=Integer.parseInt(str);
 if(num%5==0){
-if(num-5== num%10){
+tot=num-5;
+if(tot%10==0){
+
+
 System.out.println ("contiene la cifra 5");    
 }
 }
